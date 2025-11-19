1122f19be3d294cd18d6bb89b63ef8768f787959
neel <neel@chot.ai>
2025-11-19 22:06:39 +0000

    feat: stub out future materials

--- src/physics/material.rs
+++ src/physics/material.rs
@@ -4,7 +4,7 @@ use rand::{Rng, RngCore};
 pub trait Material: Send + Sync {
     fn colour(&self) -> Vec3;
 
-    fn scatter(&self, normal: Vec3, rng: &mut dyn RngCore) -> Vec3;
+    fn scatter(&self, _ray_dir: Vec3, normal: Vec3, rng: &mut dyn RngCore) -> Vec3;
 }
 
 #[derive(Debug, Clone)]
@@ -34,6 +34,56 @@ impl Material for Diffuse {
         } else {
             -in_sphere
         }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Glossy {
+    pub colour: Vec3,
+    pub roughness: f32,
+}
+
+impl Material for Glossy {
+    fn colour(&self) -> Vec3 {
+        self.colour
+    }
+
+    fn scatter(&self, _ray_dir: Vec3, _normal: Vec3, _rng: &mut dyn RngCore) -> Vec3 {
+        todo!("implement reflection with roughness/fuzz")
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Metal {
+    pub albedo: Vec3,
+    pub fuzz: f32,
+}
+
+impl Material for Metal {
+    fn colour(&self) -> Vec3 {
+        self.albedo
+    }
+
+    fn scatter(&self, _ray_dir: Vec3, _normal: Vec3, _rng: &mut dyn RngCore) -> Vec3 {
+        todo!("implement pure reflection") // (v - 2*dot(v,n)*n)
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub struct Glass {
+    pub refractive_index: f32,
+}
+
+impl Material for Glass {
+    fn colour(&self) -> Vec3 {
+        Vec3::ONE // assume glass is clear
+    }
+
+    fn scatter(&self, _ray_dir: Vec3, _normal: Vec3, _rng: &mut dyn RngCore) -> Vec3 {
+        todo!("implement refraction + specular reflection")
     }
 }
 
